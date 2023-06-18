ab6be2474c085658bfc2688327b1acc955d24642
SilentDeath53 <48599339+SilentDeath53@users.noreply.github.com>
2023-06-18 17:18:15 +0300

    Create stringutils.java

---
+++ stringutils.java
@@ -0,0 +1,9 @@
+public class StringUtils {
+    public static String reverse(String input) {
+        StringBuilder reversed = new StringBuilder();
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed.append(input.charAt(i));
+        }
+        return reversed.toString();
+    }
+}
